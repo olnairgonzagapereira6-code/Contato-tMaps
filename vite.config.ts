f2d3061a93bafb1f6efa5a406bd81ed0a45a6628
olnairgonzagapereira6-code <olnairgonzagapereira6@gmail.com>
2025-12-24 09:45:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Contato-tMaps/',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  },
+  base: '/Contato-tMaps/', // nome EXATO do repositório
 })
