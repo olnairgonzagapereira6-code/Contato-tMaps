8da6496c5fffff3359dd7b889e1ab634d1f731b7
olnairgonzagapereira6-code <olnairgonzagapereira6@gmail.com>
2025-12-26 05:55:18 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Contato-tMaps/', // nome EXATO do repositório
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
